5f6a4c82b7d67b24273e618d6d6576d7ce8b5db8
Atsushi-Momose <mmsc.am32@gmail.com>
2018-11-23 23:56:12 +0900

    2018.11.22  通信部分の実装
    Update :  youtube.api接続処理実装

---
+++ APIService/APIManager.swift
@@ -0,0 +1,46 @@
+//
+//  APIManager.swift
+//  Tubers
+//
+//  Created by mmsc on 2018/11/23.
+//  Copyright © 2018年 mmsc. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIManager: NSObject {
+    
+    private var youtubeNextPageToken: String?
+    
+    func ConnectionAPI(url: String, success: @escaping (_ result: NSArray) -> Void, failure: @escaping (Error?) -> Void) {
+
+//        let filePath = Bundle.main.path(forResource: "UrlList", ofType:"plist" )
+//        let plist = NSDictionary(contentsOfFile: filePath!)
+//        guard let url = plist!["youTubeListUrl"] as! String? else { return }
+        
+        Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: [:])
+            
+            .validate(statusCode: 200..<300)
+            .responseJSON { response in
+                switch response.result {
+                case .success(_):
+                    do {
+                        guard let data = response.data else { return }
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: data) as! NSMutableDictionary
+
+                        self.youtubeNextPageToken = jsonResult["nextPageToken"] as! String?
+                        let ary = jsonResult["items"] as! NSArray
+                        //let youtubeList = YouTubeList(ary: ary)
+                        success(ary)
+                    }
+                    catch {
+                        failure(response.error)
+                    }
+                case .failure(_):
+                    failure(response.error!)
+                }
+        }
+    }
+}
---
+++ Lists/YouTubeList.swift
@@ -0,0 +1,27 @@
+//
+//  YouTubeList.swift
+//  Tubers
+//
+//  Created by mmsc on 2018/11/23.
+//  Copyright © 2018年 mmsc. All rights reserved.
+//
+
+import Foundation
+
+class YouTubeList: NSObject {
+    
+    func loadYouTubeList() {
+        
+        let filePath = Bundle.main.path(forResource: "UrlList", ofType:"plist" )
+        let plist = NSDictionary(contentsOfFile: filePath!)
+        guard let url = plist!["youTubeListUrl"] as! String? else { return }
+        
+        APIManager().ConnectionAPI(url: url, success: {(result: NSArray) -> Void in
+            
+        }, failure: {(result: Error?) -> Void in
+            
+        })
+    }
+}
+
+

317c4d68c5239cb8a9d8419746d990f32379e28b
atsushi.momose <atsushi.momose@home-search.co.jp>
2019-06-25 14:51:38 +0900

    Youtube再生用のモーダルビュークラス追加

---
+++ Tubers/PresentationLayer/YouTube/ModalView/YoutubeChannelViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YoutubeChannelViewController.swift
+//  Tubers
+//
+//  Created by 百瀬篤志 on 2019/06/25.
+//  Copyright © 2019 mmsc. All rights reserved.
+//
+
+import UIKit
+
+class YoutubeChannelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
